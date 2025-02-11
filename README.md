194e76ba9fd6f8514fbd398246a9266adae93336
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-11 14:16:21 +0300

    2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 https://scribehow.com/shared/Githubda_Proje_Olusturma_ve_Sorunlari_Yonetme__H5vwPO2rSkmruCE-b5NUJw
 
 https://github.com/users/mancelina24/projects/2/views/3
+
+https://www.figma.com/design/tXhNJv706AWM0lXlyxLH9l/Workintech-E-commerce-UI---Figma-Ecommerce-UI-Kit-(Demo-Version)-(Community)?node-id=69-5241&p=f&t=XGHUUg078xaI1LWo-0
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
-
+import { Switch } from "react-router-dom";
 import "./App.css";
+import PageContent from "./layout/PageContent";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Pages from "./pages/Pages";
 
 function App() {
   return (
     <>
-      <h1 class="text-5xl font-bold underline">Hello world!</h1>
+      <PageContent>
+        <Header />
+        <h1 class="text-5xl font-bold underline">Hello world!</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/pages" component={Pages} />
+        </Switch>
+        <Footer />
+      </PageContent>
     </>
   );
 }
