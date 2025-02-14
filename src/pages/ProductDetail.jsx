b270cc7cdc0ecde408eaaf48658e15113b19f2d0
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-14 23:09:37 +0300

    productDetail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-
+import HeaderShop from "../layout/HeaderShop";
+import FooterShop from "../layout/FooterShop";
+import ShopLogo from "../compenents/shop/ShopLogo";
+import ProductHead from "../compenents/productDetail/producthead";
 const ProductDetail = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div>
       <HeaderShop setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
+      <ProductHead />
       <ShopLogo />
       <FooterShop />
     </div>
