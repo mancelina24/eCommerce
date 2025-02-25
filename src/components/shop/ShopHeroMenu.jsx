0039bfbfa089af2e36d1e5282e0131651889b593
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-25 15:26:42 +0300

    Refactor shop components and add new Categories component

---
+++ src/components/shop/ShopHeroMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopHeroMenu = () => {
+  return (
+    <div className="shop-hero-menu">
+      {/* Add your menu content here */}
+    </div>
+  );
+};
+
+export default ShopHeroMenu;
---
+++ src/components/shop/ShopLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopLogo = () => {
+  return (
+    <div className="shop-logo">
+      {/* Add your logo content here */}
+    </div>
+  );
+};
+
+export default ShopLogo;
