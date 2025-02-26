9fc925b0f3352ab616ed280894920fff7f2f3a55
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-26 21:20:44 +0300

    hatalı

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,7 +5,8 @@ import HeaderShop from "../layout/HeaderShop";
 import TopCategories from "../compenents/shop/TopCategories";
 import FooterShop from "../layout/FooterShop";
 import ShopLogo from "../compenents/shop/ShopLogo";
-import AllProducts from "../compenents/shop/AllProducts";
+import ShopProducts from "../compenents/shop/ShopPorducts";
+
 
 
 
